c1931ea7312cd6b90f8d6600fb4adbbd12f12421
Absharulhuda Sayed <absharulhuda@gmail.com>
2023-09-21 19:07:35 +0530

    updating

--- app/src/main/java/com/example/gstcalculator/MainActivity.kt
+++ app/src/main/java/com/example/gstcalculator/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.gstcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.example.gstcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnOne?.setOnClickListener { appendInput("1") }
+        binding.btnTwo?.setOnClickListener { appendInput("2") }
+        binding.btnThree?.setOnClickListener { appendInput("3") }
+        binding.btnFour?.setOnClickListener { appendInput("4") }
+        binding.btnFive?.setOnClickListener { appendInput("5") }
+        binding.btnSix?.setOnClickListener { appendInput("7") }
+
+        binding.btnBacksppace?.setOnClickListener {
+            handleBackspaceClick()
+        }
+
+
     }
+
+        private fun appendInput(input: String) {
+            val currentText = binding.ipCal?.text.toString()
+            val newText = currentText + input
+            binding.ipCal?.setText(newText)
+        }
+
+        private fun handleBackspaceClick() {
+            val currentText = binding.ipCal?.text.toString()
+            if (currentText.isNotEmpty()) {
+                val newText = currentText.substring(0, currentText.length - 1)
+                binding.ipCal?.setText(newText)
+            }
+        }
+
 }
+
